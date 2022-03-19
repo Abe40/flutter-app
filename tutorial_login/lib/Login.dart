2ca377bcef96abf1c828c401523dba99a62da5b1
abe <shimektabraham@gmail.com>
2022-03-19 20:00:42 +0300

    email and password feilds added

--- tutorial_login/lib/Login.dart
+++ tutorial_login/lib/Login.dart
@@ -12,7 +12,7 @@ class _LoginState extends State<Login> {
       body: SingleChildScrollView(
           child: Form(
              child:Container(
-               height:500,
+               height:600,
                width:MediaQuery.of(context).size.width,
                decoration:BoxDecoration(color:Color.fromARGB(255, 71, 211, 157),
                borderRadius:BorderRadius.only(
@@ -22,15 +22,45 @@ class _LoginState extends State<Login> {
                )),
                
     
-        child: Column(children: [
+        child: Padding(
+          padding:EdgeInsets.all(10),
+          child:Column(children: [
           SizedBox(height: 100),
           Text("Login",
               style: TextStyle(
                   fontWeight: FontWeight.bold,
                   fontSize: 50,
-                  color: Color.fromARGB(255, 0, 0, 0)))
+                  color: Color.fromARGB(255, 0, 0, 0)),
+                  ),
+                  SizedBox(height:30),
+                  Align(
+                    alignment:Alignment.topLeft,
+                  child:Text("Email",
+                  style:TextStyle(
+                    color:Colors.white,fontSize:30),
+                  )),
+                  TextFormField(
+                    style:TextStyle(fontSize:30,color:Colors.white),
+                    decoration:InputDecoration(
+                      border:OutlineInputBorder(borderSide:BorderSide.none)),),
+                      Container(height:2,
+                      color:Colors.white),
+                      SizedBox(height: 30,),
+                      Align(alignment: Alignment.topLeft,
+                      child:Text("password:",
+                      style:TextStyle(color:Colors.white,fontSize:30))),
+                      TextFormField(style:TextStyle(fontSize:30,color:Colors.white),
+                      decoration:InputDecoration(
+                        border:OutlineInputBorder(borderSide:BorderSide.none)
+                      )),
+                      Container(height:2,
+                      color:Colors.white),
+                      SizedBox(height:30),
+                      Center(child:Text("Don't have Account?",style:TextStyle(color:Colors.white,fontWeight:FontWeight.bold,fontSize:20),))
+
+            
         ]),
       )),
-    ));
+    )));
   }
 }
