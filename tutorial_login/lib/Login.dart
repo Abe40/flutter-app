4c2a316cce578b2bca2870c9e993432f332c9414
abe <shimektabraham@gmail.com>
2022-03-19 17:45:38 +0300

    login page added

--- tutorial_login/lib/Login.dart
+++ tutorial_login/lib/Login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 class Login extends StatefulWidget {
   @override
   _LoginState createState() => _LoginState();
@@ -6,15 +7,30 @@ class Login extends StatefulWidget {
 
 class _LoginState extends State<Login> {
   @override
-  Widget build(BuildContext _) {
+  Widget build(BuildContext context) {
     return Scaffold(
-      body:SingleChildScrollView(child: Form(child: Column(children: [
-        SizedBox(height:100),
-        Text("Login",
-        style:TextStyle(fontWeight:FontWeight.bold,
-        fontSize: 50,
-        color:Color.fromRGBO(233,65,82,1)))
-      ]),)) ,
-    );
+      body: SingleChildScrollView(
+          child: Form(
+             child:Container(
+               height:500,
+               width:MediaQuery.of(context).size.width,
+               decoration:BoxDecoration(color:Color.fromARGB(255, 71, 211, 157),
+               borderRadius:BorderRadius.only(
+                 bottomLeft:Radius.circular(30),
+                 bottomRight:Radius.circular(30),
+                 
+               )),
+               
+    
+        child: Column(children: [
+          SizedBox(height: 100),
+          Text("Login",
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 50,
+                  color: Color.fromARGB(255, 0, 0, 0)))
+        ]),
+      )),
+    ));
   }
 }
